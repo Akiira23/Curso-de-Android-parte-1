435074f45726e92b2d573a1c712a3c12caab3ba8
lsitec-hayashi <andre.hayashi@lsitec.org.br>
2022-05-03 16:42:49 -0300

    Adicionando ícones no projeto e Inserindo ícone e App Bar

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.agenda;
 
 import android.app.Activity;
+import android.app.AppComponentFactory;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Lista de alunos");
         setContentView(R.layout.activity_main);
         List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José", "Maria", "Ana"));
 
