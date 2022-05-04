adfb8778b23ab38e912626724e95330b658cff5a
lsitec-hayashi <andre.hayashi@lsitec.org.br>
2022-05-04 13:36:48 -0300

    personalizando layout do aluno e implementando BaseAdapter

--- app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
@@ -3,10 +3,13 @@ package com.example.agenda.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -18,6 +21,7 @@ import com.example.agenda.dao.AlunoDAO;
 import com.example.agenda.model.Aluno;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.agenda.ui.activity.ConstantesActivities.CHAVE_ALUNO;
@@ -122,8 +126,31 @@ public class ListaAlunosActivity extends AppCompatActivity {
     private void configuraAdapter(ListView listaDeAlunos) {
         adapter = new ArrayAdapter<>(
                 this,
-                android.R.layout.simple_list_item_1);
-        listaDeAlunos.setAdapter(adapter);
+                R.layout.item_aluno);
+        listaDeAlunos.setAdapter(new BaseAdapter() {
+            private final List<Aluno> alunos = new ArrayList<>();
+            @Override
+            public int getCount() {
+                return alunos.size();
+            }
+
+            @Override
+            public Aluno getItem(int i) {
+                return alunos.get(i);
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return alunos.get(i).getId();
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                View viewCriada = LayoutInflater.from(ListaAlunosActivity.this)
+                        .inflate(R.layout.item_aluno, viewGroup);
+                return viewCriada;
+            }
+        });
     }
 
 }
