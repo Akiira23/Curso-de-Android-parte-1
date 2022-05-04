c98d36edffa228750c6abf0108d7f95d38b2887b
lsitec-hayashi <andre.hayashi@lsitec.org.br>
2022-05-04 16:49:45 -0300

    utilizando o inspetor de código

--- app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,7 +1,6 @@
 package com.example.agenda.ui.activity;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
